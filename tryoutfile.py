0df60c64f4b8e35cdf37300ad646cadda9f068b5
victorcynzo <vynzonides@gmail.com>
2024-06-23 12:42:24 +0200

    to save

--- tryoutfile.py
+++ tryoutfile.py
@@ -1,8 +1,17 @@
-for x in range(12):
+#drawing two opposite circles rotating
+x = int(math.cos(ANGLE) * 200) + (SCREEN_HEIGHT/2)
+        y = int(math.sin(ANGLE) * 200) + (SCREEN_WIDTH/2)
+        pygame.draw.circle(screen, BALL_COLOUR, (x, y), BALL_RADIUS)
+
+        x_2 = int(math.cos(ANGLE + math.pi) * 200) + (SCREEN_HEIGHT/2)
+        y_2 = int(math.sin(ANGLE + math.pi) * 200) + (SCREEN_WIDTH/2)
+        pygame.draw.circle(screen, BALL_COLOUR, (x_2, y_2), BALL_RADIUS)
+
+"""for x in range(12):
     x = x +1
     print(f"x = {x}")
 
-'''import pygame, sys, math
+import pygame, sys, math
 
 run = True
 white = (255, 255, 255)

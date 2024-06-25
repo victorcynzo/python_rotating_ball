55cb7d3fd178a7488f89b86abd5ec631f23839b1
victorcynzo <vynzonides@gmail.com>
2024-06-25 11:03:30 +0200

    second_project

---
+++ spinning_2.py
@@ -0,0 +1,5 @@
+# multiple balls around each other like spinning 1
+# have all the balls move around the center point
+    # create cos sin circle ( or cos sin(sin) ) function for movement around
+# scale color of balls in relation to distance of center point, maybe also exponential (faster turning grey more on the outside)
+# scale size of balls with exponential function from center ball (faster getting smaller on the outside)
